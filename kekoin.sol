269eb88098c3b2affb19a6aef7b8080dc02baa07
artyemk <32482126+artyemk@users.noreply.github.com>
2017-11-05 12:18:54 +0500

    Update kekoin.sol

--- kekoin.sol
+++ kekoin.sol
@@ -1,5 +1,33 @@
 pragma solidity ^0.4.0;
 //import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+
+library SafeMath {
+    
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+ 
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+ 
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+ 
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+  
+}
+
+
 contract permissions
 {
     address owner = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
